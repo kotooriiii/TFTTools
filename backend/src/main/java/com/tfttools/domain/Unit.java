16b4e2c4f439cf665087bae54dd833a4b993177d
j1u <jamesquoclu@gmail.com>
2025-06-24 09:21:47 -0700

    Add documentation for all classes in backend

--- backend/src/main/java/com/tfttools/domain/Unit.java
+++ backend/src/main/java/com/tfttools/domain/Unit.java
@@ -2,10 +2,18 @@ package com.tfttools.domain;
 
 import java.util.List;
 
+/**
+ * Abstraction of a unit in Team Fight Tactics
+ */
 public final class Unit {
     private final Champion champion;
     private final List<Trait> traits;
 
+    /**
+     * Constructor for unit
+     * @param champion Champion name from enum
+     * @param traits Champion trait(s) from enum
+     */
     public Unit(Champion champion, List<Trait> traits) {
         this.champion = champion;
         this.traits = traits;
--- backend/src/main/java/com/tfttools/graph/TraitEdge.java
+++ backend/src/main/java/com/tfttools/graph/TraitEdge.java
@@ -2,9 +2,16 @@ package com.tfttools.graph;
 
 import com.tfttools.domain.Trait;
 
+/**
+ * Represents an edge in a graph, holds trait information from Units
+ */
 public class TraitEdge {
     private final Trait trait;
 
+    /**
+     * Creates a new TraitEdge with given trait
+     * @param trait Trait to be added
+     */
     public TraitEdge(Trait trait) {
         this.trait = trait;
     }
