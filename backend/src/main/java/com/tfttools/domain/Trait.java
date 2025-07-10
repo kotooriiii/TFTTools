430c887f86662ff0c91aa3e5248383ad00eae167
james <jamesquoclu@gmail.com>
2025-07-10 10:49:56 -0700

    Add trait thresholds to Trait class

--- backend/src/main/java/com/tfttools/domain/Trait.java
+++ backend/src/main/java/com/tfttools/domain/Trait.java
@@ -10,35 +10,39 @@ import java.util.stream.Collectors;
  */
 public enum Trait implements Namable
 {
-    AMP("Amp"),
-    ANIMA_SQUAD("Anima Squad"),
-    BASTION("Bastion"),
-    BOOMBOT("Boombot"),
-    BRUISER("Bruiser"),
-    CYBERBOSS("Cyberboss"),
-    CYPHER("Cypher"),
-    DIVINICORP("Divinicorp"),
-    DYNAMO("Dynamo"),
-    EXECUTIONER("Executioner"),
-    EXOTECH("Exotech"),
-    GOD_OF_THE_NET("God Of The Net"),
-    GOLDEN_OX("Golden Ox"),
-    MARKSMAN("Marksman"),
-    NITRO("Nitro"),
-    OVERLORD("Overlord"),
-    RAPIDFIRE("Rapidfire"),
-    SLAYER("Slayer"),
-    STRATEGIST("Strategist"),
-    STREET_DEMON("Street Demon"),
-    SYNDICATE("Syndicate"),
-    TECHIE("Techie"),
-    VANGUARD("Vanguard"),
-    VIRUS("Virus"),
-    SOUL_KILLER("Soul Killer");
+    AMP("Amp", new int[]{2,3,4,5}),
+    ANIMA_SQUAD("Anima Squad", new int[]{3,5,7,10}),
+    BASTION("Bastion", new int[]{2,4,6}),
+    BOOMBOT("Boombot", new int[]{2,4,6}),
+    BRUISER("Bruiser", new int[]{2,4,6}),
+    CYBERBOSS("Cyberboss", new int[]{2,3,4}),
+    CYPHER("Cypher", new int[]{3,4,5}),
+    DIVINICORP("Divinicorp", new int[]{1,2,3,4,5,6,7}),
+    DYNAMO("Dynamo", new int[]{2,3,4}),
+    EXECUTIONER("Executioner", new int[]{2,3,4,5}),
+    EXOTECH("Exotech", new int[]{3,5,7,10}),
+    GOD_OF_THE_NET("God Of The Net", new int[]{1}),
+    GOLDEN_OX("Golden Ox", new int[]{2,4,6}),
+    MARKSMAN("Marksman", new int[]{2,4}),
+    NITRO("Nitro", new int[]{3,4}),
+    OVERLORD("Overlord", new int[]{1}),
+    RAPIDFIRE("Rapidfire", new int[]{2,4,6}),
+    SLAYER("Slayer", new int[]{2,4,6}),
+    STRATEGIST("Strategist", new int[]{2,3,4,5}),
+    STREET_DEMON("Street Demon", new int[]{3,5,7,10}),
+    SYNDICATE("Syndicate", new int[]{3,5,7}),
+    TECHIE("Techie", new int[]{2,4,6,8}),
+    VANGUARD("Vanguard", new int[]{2,4,6}),
+    VIRUS("Virus", new int[]{1}),
+    SOUL_KILLER("Soul Killer", new int[]{1});
 
     private final String displayName;
+    private final int[] thresholds;
 
-    Trait(String displayName) { this.displayName = displayName; }
+    Trait(String displayName, int[] thresholds) {
+        this.displayName = displayName;
+        this.thresholds = thresholds;
+    }
 
     private static final Map<String, Trait> BY_DISPLAY_NAME = Arrays.stream(values()).collect(Collectors.toMap(Trait::getDisplayName, Function.identity()));
 
@@ -48,6 +52,10 @@ public enum Trait implements Namable
 
     public String getDisplayName() {
         return displayName;
+    }
+
+    public int[] getThresholds() {
+        return thresholds;
     }
 
     @Override
